747d6effdcc5b5ba40fc934b5cc823749b5ef06c
RBXPLAYMINI <vladatamancuk287@gmail.com>
2026-06-29 17:23:58 +0300

    Update server connection details and log messages

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const bedrock = require('bedrock-protocol');
 
 const options = {
-  host: '185.107.194.198', // Перевір, чи це актуальна IP-адреса в Атерносі
-  port: 19931,             // Перевір, чи це твій поточний порт в Атерносі
-  username: 'AternosBot247',
-  offline: true            // Це заміняє "Cracked" режим для піратів
+  host: 'surwme.aternos.me', // Твоя постійна адреса з Атерноса
+  port: 19931,               // Твій поточний порт
+  username: 'AternosBot247', // Нікнейм бота
+  offline: true,             // Працює як "Cracked" (для піратських серверів)
+  version: '1.21.30'         // Протокол підключення для версії 1.26
 };
 
 function createBot() {
@@ -13,7 +14,7 @@ function createBot() {
   const client = bedrock.createClient(options);
 
   client.on('spawn', () => {
-    console.log('Бот успішно зайшов на Bedrock сервер і з’явився у грі!');
+    console.log('Бот успішно зайшел на Bedrock сервер і з’явився у грі!');
   });
 
   client.on('error', (err) => {
